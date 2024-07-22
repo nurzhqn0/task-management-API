d667c6a6e2d5bdbb2dc35e30ecdd31a5958329b2
nurzhqn0 <nurzhqn@gmail.com>
2024-07-22 12:53:34 +0500

    feat: add hashing and salt to the password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,4 +1,5 @@
-import { DataSource, EntityRepository, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import {
   ConflictException,
@@ -17,7 +18,8 @@ export class UserRepository extends Repository<User> {
 
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
 
     try {
       await user.save();
@@ -29,5 +31,9 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
